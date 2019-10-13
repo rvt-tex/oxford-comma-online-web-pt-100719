3b83e8a5737c188cbe669b03e15b5f45fa0db18a
Ruel Tingling <rvtingling@gmail.com>
2019-10-13 01:13:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
-def oxford_comma(array)
+#def oxford_comma(array)
+ # array = []
+  
+ # if array.size == 1
+  #  return array[0]
+ # elsif array.size == 2
+  #  return array.join(" and ")
+ # else
+    #return array[0..-2].join(", ") + ", and " + array[-1]
+  #end
 
-end
+#end
+
+
+def oxford_comma(array)
+  case array.length 
+when 1
+  "#{array[0]}"
+when 2
+  array[0..1].join(" and ")
+else 
+  array[0...-1].join(", ") << ", and #{array[-1]}"
+end 
+end  
+
